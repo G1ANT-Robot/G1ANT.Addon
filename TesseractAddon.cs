49baa791c13e3a672e088166cae2387ef22e3572
Michał Adamski <michal.adamski@g1ant.com>
2017-11-21 14:41:03 +0100

    addons

---
+++ TesseractAddon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Ocr.Tesseract
+{
+    [Addon(Name = "Tesseract",
+        Tooltip = "Tesseract Commands")]
+    public class TesseractAddon : Language.Addon
+    {
+    }
+}
