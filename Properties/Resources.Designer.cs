07faa2606c972890fe34393549899d2485196780
Michał Adamski <michal.adamski@g1ant.com>
2017-11-21 15:30:29 +0100

    repairs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace G1ANT.Language.Net.Properties {
+namespace G1ANT.Addon.Net.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace G1ANT.Language.Net.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("G1ANT.Language.Net.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("G1ANT.Addon.Net.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
