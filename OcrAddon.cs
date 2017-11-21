4af0875bc880425d637a53fa84a8b79b1f6d41a2
Michał Adamski <michal.adamski@g1ant.com>
2017-11-21 14:41:03 +0100

    addons

---
+++ OcrAddon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Ocr
+{
+    [Addon(Name = "Ocr",
+        Tooltip = "Ocr Commands")]
+    public class OcrAddon : Language.Addon
+    {
+    }
+}
