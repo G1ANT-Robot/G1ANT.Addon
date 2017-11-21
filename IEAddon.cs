34ed7ac6d74a65de45aceacf15ba8f27904a3154
Michał Adamski <michal.adamski@g1ant.com>
2017-11-21 14:41:03 +0100

    addons

---
+++ IEAddon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.IExplorer
+{
+    [Addon(Name = "IExplorer",
+        Tooltip = "IExplorer Commands")]
+    public class IExplorerAddon : Language.Addon
+    {
+    }
+}
