88a060123b39a1c481ff00fdf1169f6ebf807b3b
Przemek <przemek@g1ant.com>
2017-11-21 13:09:03 +0000

    fix folder structure

---
+++ Api/InvoiceReader/PDFReader.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace G1ANT.Language.Ocr
+{
+    public class PDFReader
+    {
+        public PDFReader()
+        { }
+
+        public PDFReader(string path)
+        {
+            ReadText = GetTextFromPDF(path);
+
+        }
+
+        public string ReadText { get; }
+
+
+
+        public static string GetTextFromPDF(string path)
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+
+            return text.ToString();
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        //public static List<string> GetBetween1(string strSource, string strStart, string strEnd)
+        //{
+        //    List<string> list = new List<string>();
+        //    int Start, End;
+        //    if (!strSource.Contains(strStart) || !strSource.Contains(strEnd))
+        //    {
+        //        return list;
+        //    }
+        //    else
+        //    {
+        //        while (strSource.Contains(strStart) && strSource.Contains(strEnd))
+        //        {
+        //            Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+        //            End = strSource.IndexOf(strEnd, Start);
+        //            list.Add(strSource.Substring(Start, End - Start));
+        //            Start=Start + Start;
+        //            End = End+End;
+                    
+        //            //return strSource.Substring(Start, End - Start);
+        //        }
+        //        return list;
+        //    }
+        //}
+
+
+        public static List<string> lGetTextFromPDF(string path)
+        {
+            List<string> text = new List<string>();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    
+                    text.Add(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+
+            return text;
+        }
+
+    }
+}
---
+++ Commands/OcrFindPointCommand.cs
@@ -0,0 +1,47 @@
+﻿using G1ANT.Language;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace G1ANT.Language.Ocr
+{
+    [Command(Name = "ocr.findpoint", Tooltip = "This command allows to find the text on the active screen and return it's position in a 'point' format. ", IsUnderConstruction = true)]
+    public class OcrFindPointCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true,Tooltip = "Text that you want to find in the screen. Provide only single words")]
+            public TextStructure Search { get; set; }
+
+            [Argument]
+            public RectangleStructure Area { get; set; } = new RectangleStructure(System.Windows.Forms.Screen.PrimaryScreen.Bounds);
+
+            [Argument]
+            public BooleanStructure Relative { get; set; } = new BooleanStructure(true);
+
+            [Argument]
+            public TextStructure Result { get; set; } = new TextStructure("result");
+
+            [Argument(DefaultVariable = "timeoutOcr")]
+            public override int Timeout { get; set; }
+
+            [Argument(Tooltip = "Comma separated list of languages from which you want to recognize text on the screen")]
+            public TextStructure Languages { get; set; } = new TextStructure("en");
+        }
+        public OcrFindPointCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            System.Drawing.Rectangle rectangle = arguments.Relative.Value ? Helpers.ToAbsoluteCoordinates(arguments.Area.Value) : arguments.Area.Value;
+            System.Drawing.Bitmap partOfScreen = RobotWin32.GetPartOfScreen(rectangle);
+            int timeout = arguments.Timeout;
+            List<string> languages = arguments.Languages.Value.Split(',').ToList();
+            string search = arguments.Search.Value;
+            GoogleCloudApi googleApi = new GoogleCloudApi();
+
+            System.Drawing.Rectangle output = googleApi.RecognizeText(partOfScreen, search, languages, timeout);
+            System.Drawing.Point pointOutput = new System.Drawing.Point(output.X + arguments.Area.Value.X, output.Y + arguments.Area.Value.Y);
+            Scripter.Variables.SetVariableValue(arguments.Result.Value, new PointStructure(pointOutput));
+        }
+    }
+}
