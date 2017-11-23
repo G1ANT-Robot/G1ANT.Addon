b3850b9f6725a256f934dd3aefe9b5779b1b63ef
Michał Adamski <michal.adamski@g1ant.com>
2017-11-23 16:35:32 +0100

    selenium repairs

--- G1ANT.Addon.Selenium.Tests/UnitTest1.cs
+++ G1ANT.Addon.Selenium.Tests/UnitTest1.cs
@@ -4,10 +4,10 @@ using NUnit.Framework;
 namespace G1ANT.Addon.Selenium.Tests
 {
     [TestFixture]
-    public class UnitTest1
+    public class SeleniumLoadTests
     {
         [Test]
-        public void LoadMSOfficeAddon()
+        public void LoadSelleniumAddon()
         {
             Language.Addon addon = Language.Addon.Load("G1ANT.Addon.Selenium.dll");
             Assert.IsNotNull(addon);
