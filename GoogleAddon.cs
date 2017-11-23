212dfe0b470a7fbd8288bd6cc8aacfb16cbf6dde
Michał Adamski <michal.adamski@g1ant.com>
2017-11-23 16:16:22 +0100

    addons repair

---
+++ GoogleAddon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.GoogleDocs
+{
+    [Addon(Name = "GoogleDocs",
+        Tooltip = "GoogleDocs Commands")]
+    [CommandGroup(Name = "googlesheet", Tooltip = "Command connected with creating, editing and generally working on GoogleDocs")]
+    public class GoogleAddon : Language.Addon
+    {
+    }
+}
