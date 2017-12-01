7bb4afebecf5bf1168179a44440d29d5f91776f3
Michał Adamski <michal.adamski@g1ant.com>
2017-12-01 11:08:07 +0100

    Pdf Addon

---
+++ G1ANT.Addon.Pdf/PdfAddon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Pdf
+{
+    [Addon(Name = "Pdf",
+        Tooltip = "Pdf Commands")]
+    [CommandGroup(Name = "pdf", Tooltip = "Addon to work with PDF files")]
+    public class PdfAddon : Language.Addon
+    {
+    }
+}
