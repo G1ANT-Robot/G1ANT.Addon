63068a1bafedacb5d6aac009685e8c06eca676d5
zuzaszafran <zuza.szafranowska@g1ant.com>
2017-12-13 09:28:34 +0100

    tttt

--- G1ANT.Addon.MSOffice/Commands/ExcelGetRowCommand.cs
+++ G1ANT.Addon.MSOffice/Commands/ExcelGetRowCommand.cs
@@ -26,7 +26,7 @@ namespace G1ANT.Addon.MSOffice
                 var val = ExcelManager.CurrentExcel.GetRow(arguments.Row.Value); 
                 var structureDictionary = val.ToDictionary(x => x.Key, x => (Structure)new TextStructure(x.Value));
                 Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(structureDictionary)); 
-            }
+            } 
             catch (Exception ex)
             {
                 throw new ApplicationException($"Problem occured while getting row: '{arguments.Row.Value}'. Message: {ex.Message}", ex);
