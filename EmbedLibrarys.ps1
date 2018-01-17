b07ce2c03a6513afaa042154198375c1a87c73e3
Marcin Machowski <marcin.machowski@g1ant.com>
2018-01-17 10:34:29 +0100

    Selecting librarys to embed, parial implementation

--- EmbedLibrarys.ps1
+++ EmbedLibrarys.ps1
@@ -1,3 +1,16 @@
 $SolutionDir = Split-Path $Script:MyInvocation.MyCommand.Path
 cd $SolutionDir
 Write-Host "$pwd"
+
+[xml]$ProjFile = Get-Content -Path G1ANT.Addon.MSOffice\G1ANT.Addon.MSOffice.csproj
+$ref = @()
+$ProjFile.Project.ItemGroup.Reference | foreach{$ref+=$_}
+$toEmbed = @()
+foreach($r in $ref)
+{
+	if($r.HintPath -and (!$r.SpecificVersion -or ($r.SpecificVersion -eq $true)))
+	{
+		$toEmbed += $r
+	}
+}
+Write-Host $toEmbed
