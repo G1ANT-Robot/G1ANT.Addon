9ebffe3eceb2101f100b1924f0c487401b4db17d
jan.bujanowski@g1ant.com <jan.bujanowski@g1ant.com>
2018-01-24 16:13:04 +0100

    rename and replace projects related to commands xls (chhange to XLSX)

---
+++ Commands/XlsCloseCommand.cs
@@ -0,0 +1,31 @@
+﻿using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.Xls
+{
+    [Command(Name = "xls.close",Tooltip = "This command allows to save changes and close .xlsx file.")]
+    public class XlsCloseCommand :Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Tooltip = "ID of file to close. If not set, will close file opened as first")]
+            public IntegerStructure Id { get; set; }
+        }
+        public XlsCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            int ID;
+            if (arguments.Id == null)
+            {
+                ID = XlsManager.getFirstId();
+            }
+            else
+            {
+                ID = arguments.Id.Value;
+            }
+            XlsManager.Remove(ID);
+        }
+    }
+}
---
+++ Commands/XlsSwitchCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Xls
+{
+    [Command(Name = "xls.switch", Tooltip = "This command allows to switch between open .xlsx files.")]
+    public class XlsSwitchCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true)]
+            public IntegerStructure Id { get; set; } = new IntegerStructure(0);
+
+            [Argument]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+        public XlsSwitchCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                int id = arguments.Id.Value;
+                bool result = XlsManager.SwitchXls(id);
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new BooleanStructure(result));
+            }
+            catch
+            {
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new BooleanStructure(false));
+                throw new ApplicationException("Specified Xls not existing");
+            }
+        }
+    }
+}
