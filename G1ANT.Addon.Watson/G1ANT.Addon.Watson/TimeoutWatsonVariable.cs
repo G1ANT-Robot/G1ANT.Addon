34a0379cbb278a7f71320e4f132a2cae6f368b80
Marek Kosinski <marek.kosinski@g1ant.com>
2019-03-28 09:08:03 +0100

    Added watson timeout variable

---
+++ G1ANT.Addon.Watson/G1ANT.Addon.Watson/TimeoutWatsonVariable.cs
@@ -0,0 +1,31 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Watson
+{
+    [Variable(
+        Name = "timeoutwatson",
+        Tooltip = "Determines the timeout value for the watson. commands; the default value is 60000 ms.")]
+    public class TimeoutWatsonVariable : Variable
+    {
+        private TimeSpanStructure value;
+        public TimeoutWatsonVariable(AbstractScripter scripter = null) : base(scripter)
+        {
+            value = new TimeSpanStructure(new TimeSpan(0, 0, 60), "", scripter);
+        }
+
+        public override Structure GetValue(string index = null)
+        {
+            return value.Get(index);
+        }
+
+        public override void SetValue(Structure value, string index = null)
+        {
+            this.value.Set(value, index);
+        }
+    }
+}
