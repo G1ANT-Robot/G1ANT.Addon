b1e7af2de7edcdf2a5ccee518605c06ae6a09957
DanielWilczek <daniel.wilczek@g1ant.com>
2019-07-03 13:23:52 +0200

    on activatetab switch CurrentIE to activated tab

--- G1ANT.Addon.IExplorer/G1ANT.Addon.IExplorer/Api/TabActivator.cs
+++ G1ANT.Addon.IExplorer/G1ANT.Addon.IExplorer/Api/TabActivator.cs
@@ -10,6 +10,7 @@
 using Accessibility;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace G1ANT.Addon.IExplorer
@@ -154,6 +155,8 @@ namespace G1ANT.Addon.IExplorer
                         if (index == tabIndexToActivate)
                         {
                             tab.ActivateTab();
+                            var activeIe = IEManager.launchedIE?.Where(ie => ie.Ie.Url == tab.LocationUrl).FirstOrDefault();
+                            if (activeIe != null) IEManager.SwitchIE(activeIe.Id);
                             return;
                         }
 
